a81f19a51172365279e67dc56785356c883ef1b8
Frederik Riedel <frederik@riedel.wtf>
2025-07-19 00:21:58 +0200

    remove fredkit from openresearchkit

--- Sources/OpenResearchKit/Views/StudyBannerInvitation.swift
+++ Sources/OpenResearchKit/Views/StudyBannerInvitation.swift
@@ -8,7 +8,6 @@
 import SafariServices
 import SwiftUI
 import UIKit
-import FredKit
 
 public struct StudyBannerInvitation: View {
     
@@ -58,7 +57,8 @@ public struct StudyBannerInvitation: View {
                     .foregroundColor(.primary)
                     .font(.headline)
                     .bold()
-                Text("Thanks a lot for participating in the study, the \(study.duration.humanReadableTimeInterval) of participation are now completed. Please fill out one last 3 minute survey!")
+                let studyDurationWeekCount = Int(study.duration / 604800.0)
+                Text("Thanks a lot for participating in the study, the \(studyDurationWeekCount) weeks of participation are now completed. Please fill out one last 3 minute survey!")
                     .foregroundColor(.secondary)
             }
             
