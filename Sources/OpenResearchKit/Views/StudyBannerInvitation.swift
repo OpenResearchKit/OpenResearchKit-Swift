24c0ca8a9291747387a00f9bf382c365b0c081b6
Lennart Fischer <github@lambdadigamma.com>
2025-08-28 12:17:06 +0200

    fixed build error

--- Sources/OpenResearchKit/Views/StudyBannerInvitation.swift
+++ Sources/OpenResearchKit/Views/StudyBannerInvitation.swift
@@ -81,27 +81,3 @@ public struct StudyBannerInvitation: View {
         }
     }
 }
-
-
-struct BigButtonStyle: ButtonStyle {
-    
-    let backgroundColor: Color
-    let textColor: Color
-    
-    func makeBody(configuration: Configuration) -> some View {
-        
-        HStack {
-            Spacer()
-            configuration.label
-                .font(.body.weight(.semibold))
-                .foregroundColor(textColor)
-            Spacer()
-        }
-        .padding(16)
-        .background(backgroundColor)
-        .mask(RoundedRectangle(cornerRadius: 12))
-        .opacity(configuration.isPressed ? 0.6 : 1)
-        .animation(.easeOut(duration: 0.1), value: configuration.isPressed)
-    }
-    
-}
