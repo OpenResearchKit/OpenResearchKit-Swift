bc9769d5f01d3c2107082f5018bedde9e79a0357
Lennart Fischer <info@lambdadigamma.com>
2026-07-11 00:35:52 +0200

    chore: add `StudyDetailDebugAlert`

---
+++ Sources/OpenResearchKit/Views/Settings/StudyDetailAlert.swift
@@ -0,0 +1,16 @@
+//
+//  StudyDetailDebugAlert.swift
+//  OpenResearchKit
+//
+//  Created by Lennart Fischer on 11.07.26.
+//
+
+import Foundation
+
+struct StudyDetailDebugAlert: Identifiable {
+
+    let id = UUID()
+    let title: String
+    let message: String
+
+}
